f2ff0578e4c45aa9fbcecadae8c65ecb3a30d3fa
Tom Herbers <mail@tomherbers.de>
2022-02-02 15:51:45 +0100

    js: map: switch to FFRN Tile Mirror

--- static/js/View/map.js
+++ static/js/View/map.js
@@ -21,9 +21,10 @@ app.View.Map = function ($, mapID) {
       map = L.map(mapID, options).setView([49.47704787438876, 8.5638427734375]);
 
       // + Set the tile Layer and add to map.
-      //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      // L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-      L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      //L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      //L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      L.tileLayer('https://tiles.ffrn.de/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors <br/> Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
       }).addTo(map);
 
